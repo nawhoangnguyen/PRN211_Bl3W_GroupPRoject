651f984e2b0ca7bba6747f99b61c464f5bd3bc01
leehai1107 <leehai1107@gmail.com>
2023-11-17 00:44:36 +0700

    add discount

---
+++ SalesWinApp/frmDiscount.cs
@@ -0,0 +1,129 @@
+﻿using BusinessObject.Models;
+using DataAccess;
+using DataAccess.Repository;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SalesWinApp
+{
+    public partial class frmDiscount : Form
+    {
+        IDiscountRepository discountRepository = new DiscountRepository();
+        IProductRepository productRepository = new ProductRepository();
+        BindingSource source;
+        public bool isAdmin { get; set; }
+
+        public int memberId { get; set; }
+
+        public frmDiscount()
+        {
+            InitializeComponent();
+        }
+
+        public void LoadDiscount(List<DiscountDTO> discountDTOs)
+        {
+            try
+            {
+                txtID.Enabled= false;
+                txtName.Enabled= false;
+                txtDescript.Enabled= false;
+                txtPercent.Enabled= false;
+                List<DiscountDTO> discounts = new List<DiscountDTO>();
+                if (discountDTOs.IsNullOrEmpty())
+                {
+                    discounts = discountRepository.GetAll();
+
+                }
+                else
+                {
+                    discounts = discountDTOs;
+                }
+
+                source = new BindingSource();
+                source.DataSource = discounts;
+                txtID.DataBindings.Clear();
+                txtName.DataBindings.Clear();
+                txtDescript.DataBindings.Clear();
+                txtPercent.DataBindings.Clear();
+
+                txtID.DataBindings.Add("Text", source, "DiscountId");
+                txtName.DataBindings.Add("Text", source, "DiscountName");
+                txtDescript.DataBindings.Add("Text",source, "Description");
+                txtPercent.DataBindings.Add("Text", source, "DiscountPercent");
+
+                dataGridView1.DataSource = null;
+                dataGridView1.DataSource = source;
+
+                dataGridView1.Columns["Products"].Visible=false;
+
+
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+
+        private void frmDiscount_Load(object sender, EventArgs e)
+        {
+            txtID.Enabled = false;
+            txtName.Enabled = false;
+            txtDescript.Enabled = false;
+            txtPercent.Enabled = false;
+
+            LoadDiscount(null);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            frmDiscountDetail frm = new frmDiscountDetail()
+            {
+                action = false,
+                idToUpdate = Int32.Parse(txtID.Text)
+                
+            };
+            if (frm.ShowDialog() == DialogResult.OK)
+            {
+                LoadDiscount(null);
+                source.Position = source.Count - 1;
+            }
+            LoadDiscount(null);
+            source.Position = source.Count - 1;
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            frmDiscountDetail frm = new frmDiscountDetail()
+            {
+                action = true
+
+            };
+            if (frm.ShowDialog() == DialogResult.OK)
+            {
+                LoadDiscount(null);
+                source.Position = source.Count - 1;
+            }
+            LoadDiscount(null);
+            source.Position = source.Count - 1;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Do you sure you want to delete this discount ?", "Confirm", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.Yes)
+            {
+                discountRepository.Delete(Int32.Parse(txtID.Text));
+                LoadDiscount(null);
+            }
+        }
+    }
+}
---
+++ SalesWinApp/frmDiscountDetail.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace SalesWinApp
+{
+    partial class frmDiscountDetail
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label7 = new System.Windows.Forms.Label();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtDescription = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtDiscountName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtDiscountId = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPercent = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.txtPercent)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label7.Location = new System.Drawing.Point(279, 22);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(247, 41);
+            this.label7.TabIndex = 35;
+            this.label7.Text = "Discount Details";
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(530, 371);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(133, 41);
+            this.btnCancel.TabIndex = 49;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(127, 371);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(133, 41);
+            this.btnSave.TabIndex = 48;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(24, 282);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(94, 22);
+            this.label5.TabIndex = 44;
+            this.label5.Text = "Percent";
+            // 
+            // txtDescription
+            // 
+            this.txtDescription.Location = new System.Drawing.Point(190, 217);
+            this.txtDescription.Multiline = true;
+            this.txtDescription.Name = "txtDescription";
+            this.txtDescription.Size = new System.Drawing.Size(536, 41);
+            this.txtDescription.TabIndex = 43;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(24, 219);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(142, 22);
+            this.label4.TabIndex = 42;
+            this.label4.Text = "Description";
+            // 
+            // txtDiscountName
+            // 
+            this.txtDiscountName.Location = new System.Drawing.Point(190, 156);
+            this.txtDiscountName.Multiline = true;
+            this.txtDiscountName.Name = "txtDiscountName";
+            this.txtDiscountName.Size = new System.Drawing.Size(536, 41);
+            this.txtDiscountName.TabIndex = 41;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(24, 158);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(154, 22);
+            this.label3.TabIndex = 40;
+            this.label3.Text = "DiscountName";
+            // 
+            // txtDiscountId
+            // 
+            this.txtDiscountId.Location = new System.Drawing.Point(190, 86);
+            this.txtDiscountId.Multiline = true;
+            this.txtDiscountId.Name = "txtDiscountId";
+            this.txtDiscountId.Size = new System.Drawing.Size(536, 41);
+            this.txtDiscountId.TabIndex = 37;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(24, 88);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(142, 22);
+            this.label1.TabIndex = 36;
+            this.label1.Text = "Discount ID";
+            // 
+            // txtPercent
+            // 
+            this.txtPercent.DecimalPlaces = 1;
+            this.txtPercent.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.txtPercent.Location = new System.Drawing.Point(190, 283);
+            this.txtPercent.Maximum = new decimal(new int[] {
+            90,
+            0,
+            0,
+            0});
+            this.txtPercent.Name = "txtPercent";
+            this.txtPercent.Size = new System.Drawing.Size(536, 27);
+            this.txtPercent.TabIndex = 50;
+            // 
+            // DiscountDetail
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(804, 454);
+            this.Controls.Add(this.txtPercent);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtDescription);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtDiscountName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtDiscountId);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label7);
+            this.Name = "DiscountDetail";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DiscountDetail";
+            this.Load += new System.EventHandler(this.DiscountDetail_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.txtPercent)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private Label label7;
+        private Button btnCancel;
+        private Button btnSave;
+        private Label label5;
+        private TextBox txtDescription;
+        private Label label4;
+        private TextBox txtDiscountName;
+        private Label label3;
+        private TextBox txtDiscountId;
+        private Label label1;
+        private NumericUpDown txtPercent;
+    }
+}
