5a77dd7aea3c8db7a29ff107791ace3241039928
nguyenthanh311003 <nguyenthanh311003@gmail.com>
2023-11-16 16:29:14 +0700

    member detail ui

--- SalesWinApp/frmMembers.Designer.cs
+++ SalesWinApp/frmMembers.Designer.cs
@@ -47,12 +47,12 @@
             label7 = new Label();
             groupBox1 = new GroupBox();
             groupBox2 = new GroupBox();
-            label8 = new Label();
-            textBox1 = new TextBox();
-            button1 = new Button();
-            label9 = new Label();
-            textBox2 = new TextBox();
             button2 = new Button();
+            textBox2 = new TextBox();
+            label9 = new Label();
+            button1 = new Button();
+            textBox1 = new TextBox();
+            label8 = new Label();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             groupBox1.SuspendLayout();
             groupBox2.SuspendLayout();
@@ -60,97 +60,109 @@
             // 
             // txtPassword
             // 
-            txtPassword.Location = new Point(602, 213);
+            txtPassword.Location = new Point(633, 210);
+            txtPassword.Multiline = true;
             txtPassword.Name = "txtPassword";
-            txtPassword.Size = new Size(210, 27);
+            txtPassword.Size = new Size(210, 41);
             txtPassword.TabIndex = 47;
             // 
             // label6
             // 
             label6.AutoSize = true;
-            label6.Location = new Point(526, 217);
+            label6.Font = new Font("Courier New", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label6.Location = new Point(510, 214);
             label6.Name = "label6";
-            label6.Size = new Size(70, 20);
+            label6.Size = new Size(106, 22);
             label6.TabIndex = 46;
             label6.Text = "Password";
             // 
             // txtCountry
             // 
-            txtCountry.Location = new Point(602, 145);
+            txtCountry.Location = new Point(633, 143);
+            txtCountry.Multiline = true;
             txtCountry.Name = "txtCountry";
-            txtCountry.Size = new Size(210, 27);
+            txtCountry.Size = new Size(210, 41);
             txtCountry.TabIndex = 45;
             // 
             // label5
             // 
             label5.AutoSize = true;
-            label5.Location = new Point(526, 147);
+            label5.Font = new Font("Courier New", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label5.Location = new Point(510, 151);
             label5.Name = "label5";
-            label5.Size = new Size(60, 20);
+            label5.Size = new Size(94, 22);
             label5.TabIndex = 44;
             label5.Text = "Country";
             // 
             // txtCity
             // 
-            txtCity.Location = new Point(600, 82);
+            txtCity.Location = new Point(633, 84);
+            txtCity.Multiline = true;
             txtCity.Name = "txtCity";
-            txtCity.Size = new Size(210, 27);
+            txtCity.Size = new Size(210, 41);
             txtCity.TabIndex = 43;
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(526, 85);
+            label4.Font = new Font("Courier New", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label4.Location = new Point(510, 92);
             label4.Name = "label4";
-            label4.Size = new Size(34, 20);
+            label4.Size = new Size(58, 22);
             label4.TabIndex = 42;
             label4.Text = "City";
             // 
             // txtCompanyName
             // 
-            txtCompanyName.Location = new Point(206, 213);
+            txtCompanyName.Location = new Point(220, 210);
+            txtCompanyName.Multiline = true;
             txtCompanyName.Name = "txtCompanyName";
-            txtCompanyName.Size = new Size(210, 27);
+            txtCompanyName.Size = new Size(210, 41);
             txtCompanyName.TabIndex = 41;
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(88, 217);
+            label3.Font = new Font("Courier New", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.Location = new Point(72, 214);
             label3.Name = "label3";
-            label3.Size = new Size(112, 20);
+            label3.Size = new Size(142, 22);
             label3.TabIndex = 40;
             label3.Text = "CompanyName";
             // 
             // txtEmail
             // 
-            txtEmail.Location = new Point(206, 141);
+            txtEmail.Location = new Point(220, 143);
+            txtEmail.Multiline = true;
             txtEmail.Name = "txtEmail";
-            txtEmail.Size = new Size(210, 27);
+            txtEmail.Size = new Size(210, 41);
             txtEmail.TabIndex = 39;
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(88, 147);
+            label2.Font = new Font("Courier New", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(72, 151);
             label2.Name = "label2";
-            label2.Size = new Size(46, 20);
+            label2.Size = new Size(70, 22);
             label2.TabIndex = 38;
             label2.Text = "Email";
             // 
             // txtMemberId
             // 
-            txtMemberId.Location = new Point(206, 78);
+            txtMemberId.Location = new Point(220, 84);
+            txtMemberId.Multiline = true;
             txtMemberId.Name = "txtMemberId";
-            txtMemberId.Size = new Size(210, 27);
+            txtMemberId.Size = new Size(210, 41);
             txtMemberId.TabIndex = 37;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(88, 82);
+            label1.Font = new Font("Courier New", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(72, 92);
             label1.Name = "label1";
-            label1.Size = new Size(78, 20);
+            label1.Size = new Size(106, 22);
             label1.TabIndex = 36;
             label1.Text = "MemberId";
             // 
@@ -200,7 +212,7 @@
             // 
             label7.AutoSize = true;
             label7.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point);
-            label7.Location = new Point(395, 9);
+            label7.Location = new Point(405, 9);
             label7.Name = "label7";
             label7.Size = new Size(137, 41);
             label7.TabIndex = 48;
@@ -234,31 +246,22 @@
             groupBox2.TabStop = false;
             groupBox2.Text = "Search";
             // 
-            // label8
+            // button2
             // 
-            label8.AutoSize = true;
-            label8.Location = new Point(9, 41);
-            label8.Name = "label8";
-            label8.Size = new Size(52, 20);
-            label8.TabIndex = 0;
-            label8.Text = "Name:";
+            button2.Location = new Point(270, 71);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 5;
+            button2.Text = "Search";
+            button2.UseVisualStyleBackColor = true;
             // 
-            // textBox1
+            // textBox2
             // 
-            textBox1.Location = new Point(67, 38);
-            textBox1.Name = "textBox1";
-            textBox1.PlaceholderText = "Member name...";
-            textBox1.Size = new Size(185, 27);
-            textBox1.TabIndex = 1;
-            // 
-            // button1
-            // 
-            button1.Location = new Point(270, 36);
-            button1.Name = "button1";
-            button1.Size = new Size(94, 29);
-            button1.TabIndex = 2;
-            button1.Text = "Search";
-            button1.UseVisualStyleBackColor = true;
+            textBox2.Location = new Point(67, 71);
+            textBox2.Name = "textBox2";
+            textBox2.PlaceholderText = "City...";
+            textBox2.Size = new Size(185, 27);
+            textBox2.TabIndex = 4;
             // 
             // label9
             // 
@@ -270,22 +273,31 @@
             label9.Text = "City:";
             label9.Click += label9_Click;
             // 
-            // textBox2
+            // button1
             // 
-            textBox2.Location = new Point(67, 71);
-            textBox2.Name = "textBox2";
-            textBox2.PlaceholderText = "City...";
-            textBox2.Size = new Size(185, 27);
-            textBox2.TabIndex = 4;
+            button1.Location = new Point(270, 36);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 2;
+            button1.Text = "Search";
+            button1.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // textBox1
             // 
-            button2.Location = new Point(270, 71);
-            button2.Name = "button2";
-            button2.Size = new Size(94, 29);
-            button2.TabIndex = 5;
-            button2.Text = "Search";
-            button2.UseVisualStyleBackColor = true;
+            textBox1.Location = new Point(67, 38);
+            textBox1.Name = "textBox1";
+            textBox1.PlaceholderText = "Member name...";
+            textBox1.Size = new Size(185, 27);
+            textBox1.TabIndex = 1;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(9, 41);
+            label8.Name = "label8";
+            label8.Size = new Size(52, 20);
+            label8.TabIndex = 0;
+            label8.Text = "Name:";
             // 
             // frmMembers
             // 
