1629f24d9fa17e45bd98f689a0430e9b5bc0cb70
nguyenthanh311003 <nguyenthanh311003@gmail.com>
2023-11-16 16:01:06 +0700

    login ui

--- SalesWinApp/frmLogin.Designer.cs
+++ SalesWinApp/frmLogin.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmLogin));
             label1 = new Label();
             label2 = new Label();
             label3 = new Label();
@@ -35,86 +36,170 @@
             txtPassword = new TextBox();
             btnLogin = new Button();
             btnCancel = new Button();
+            panel1 = new Panel();
+            panel3 = new Panel();
+            label4 = new Label();
+            panel2 = new Panel();
+            pictureBox1 = new PictureBox();
+            panel1.SuspendLayout();
+            panel3.SuspendLayout();
+            panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI Historic", 18F, FontStyle.Bold, GraphicsUnit.Point);
-            label1.Location = new Point(341, 64);
+            label1.Location = new Point(256, 499);
             label1.Name = "label1";
-            label1.Size = new Size(88, 32);
+            label1.Size = new Size(111, 41);
             label1.TabIndex = 0;
             label1.Text = "LOGIN";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(157, 159);
+            label2.Font = new Font("Courier New", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(44, 121);
             label2.Name = "label2";
-            label2.Size = new Size(36, 15);
+            label2.Size = new Size(70, 22);
             label2.TabIndex = 1;
             label2.Text = "Email";
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(157, 208);
+            label3.Font = new Font("Courier New", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.Location = new Point(44, 231);
             label3.Name = "label3";
-            label3.Size = new Size(57, 15);
+            label3.Size = new Size(106, 22);
             label3.TabIndex = 2;
             label3.Text = "Password";
             // 
             // txtEmail
             // 
-            txtEmail.Location = new Point(266, 159);
+            txtEmail.Location = new Point(44, 154);
+            txtEmail.Margin = new Padding(3, 4, 3, 4);
+            txtEmail.Multiline = true;
             txtEmail.Name = "txtEmail";
-            txtEmail.Size = new Size(258, 23);
+            txtEmail.Size = new Size(408, 45);
             txtEmail.TabIndex = 3;
             // 
             // txtPassword
             // 
-            txtPassword.Location = new Point(266, 200);
+            txtPassword.Location = new Point(44, 268);
+            txtPassword.Margin = new Padding(3, 4, 3, 4);
+            txtPassword.Multiline = true;
             txtPassword.Name = "txtPassword";
             txtPassword.PasswordChar = '*';
-            txtPassword.Size = new Size(258, 23);
+            txtPassword.Size = new Size(408, 44);
             txtPassword.TabIndex = 4;
             // 
             // btnLogin
             // 
-            btnLogin.Location = new Point(354, 254);
+            btnLogin.BackColor = Color.FromArgb(199, 220, 167);
+            btnLogin.Font = new Font("Courier New", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            btnLogin.Location = new Point(44, 347);
+            btnLogin.Margin = new Padding(3, 4, 3, 4);
             btnLogin.Name = "btnLogin";
-            btnLogin.Size = new Size(75, 23);
+            btnLogin.Size = new Size(408, 51);
             btnLogin.TabIndex = 5;
-            btnLogin.Text = "Login";
-            btnLogin.UseVisualStyleBackColor = true;
+            btnLogin.Text = "LOGIN";
+            btnLogin.UseVisualStyleBackColor = false;
             btnLogin.Click += btnLogin_Click;
             // 
             // btnCancel
             // 
-            btnCancel.Location = new Point(354, 401);
+            btnCancel.BackColor = Color.FromArgb(199, 220, 167);
+            btnCancel.Font = new Font("Courier New", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            btnCancel.Location = new Point(403, 459);
+            btnCancel.Margin = new Padding(3, 4, 3, 4);
             btnCancel.Name = "btnCancel";
-            btnCancel.Size = new Size(75, 23);
+            btnCancel.Size = new Size(86, 31);
             btnCancel.TabIndex = 6;
             btnCancel.Text = "Close";
-            btnCancel.UseVisualStyleBackColor = true;
+            btnCancel.UseVisualStyleBackColor = false;
             btnCancel.Click += btnCancel_Click;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.White;
+            panel1.Controls.Add(panel3);
+            panel1.Controls.Add(panel2);
+            panel1.Location = new Point(170, 88);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(971, 504);
+            panel1.TabIndex = 7;
+            // 
+            // panel3
+            // 
+            panel3.BackColor = Color.White;
+            panel3.Controls.Add(label4);
+            panel3.Controls.Add(txtEmail);
+            panel3.Controls.Add(btnCancel);
+            panel3.Controls.Add(label2);
+            panel3.Controls.Add(btnLogin);
+            panel3.Controls.Add(label3);
+            panel3.Controls.Add(txtPassword);
+            panel3.Dock = DockStyle.Right;
+            panel3.Location = new Point(468, 0);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(503, 504);
+            panel3.TabIndex = 1;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Courier New", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            label4.Location = new Point(210, 67);
+            label4.Name = "label4";
+            label4.Size = new Size(105, 34);
+            label4.TabIndex = 7;
+            label4.Text = "Login";
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(pictureBox1);
+            panel2.Dock = DockStyle.Left;
+            panel2.Location = new Point(0, 0);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(468, 504);
+            panel2.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Dock = DockStyle.Fill;
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(0, 0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(468, 504);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
             // 
             // frmLogin
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(btnCancel);
-            Controls.Add(btnLogin);
-            Controls.Add(txtPassword);
-            Controls.Add(txtEmail);
-            Controls.Add(label3);
-            Controls.Add(label2);
+            BackColor = Color.BlanchedAlmond;
+            ClientSize = new Size(1315, 780);
+            Controls.Add(panel1);
             Controls.Add(label1);
+            ForeColor = SystemColors.ControlText;
+            Margin = new Padding(3, 4, 3, 4);
+            MaximizeBox = false;
+            MinimizeBox = false;
+            MinimumSize = new Size(1333, 827);
             Name = "frmLogin";
+            StartPosition = FormStartPosition.CenterScreen;
             Text = "Login";
             Load += frmLogin_Load;
+            panel1.ResumeLayout(false);
+            panel3.ResumeLayout(false);
+            panel3.PerformLayout();
+            panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -128,5 +213,10 @@
         private TextBox txtPassword;
         private Button btnLogin;
         private Button btnCancel;
+        private Panel panel1;
+        private Panel panel3;
+        private Panel panel2;
+        private PictureBox pictureBox1;
+        private Label label4;
     }
 }
